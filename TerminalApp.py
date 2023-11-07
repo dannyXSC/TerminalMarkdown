74a1b3ef1efd7b86293e163d3152137fdfc13a39
zb2313 <2224105295@qq.com>
2023-11-07 12:17:38 +0800

    自动测试

--- TerminalApp.py
+++ TerminalApp.py
@@ -1,9 +1,12 @@
+import time
+
 from Application import Application
-from DerivedCommand import InsertRowCommand, InsertHeadCommand, InsertTailCommand, DeleteRowCommand, DeleteTextCommand,\
-                    ListCommand, HistoryCommand,ListTreeCommand,DirTreeCommand
+from DerivedCommand import InsertRowCommand, InsertHeadCommand, InsertTailCommand, DeleteRowCommand, DeleteTextCommand, \
+    ListCommand, HistoryCommand, ListTreeCommand, DirTreeCommand
 from SimpleMarkdownDoc import SimpleMarkdownDoc
 from Logger import Logger
 from Sessioner import Session
+
 
 class TerminalApp(Application):
     def __init__(self, cmd_capacity=50):
@@ -21,7 +24,7 @@ class TerminalApp(Application):
     def InsertHead(self, text):
         self._cmdManager.Execute(InsertHeadCommand(self._activateDoc, text))
 
-    def InsertTail(self,text):
+    def InsertTail(self, text):
         self._cmdManager.Execute(InsertTailCommand(self._activateDoc, text))
 
     def DeleteRow(self, row_num):
@@ -35,9 +38,9 @@ class TerminalApp(Application):
 
     def ListTree(self):
         self._cmdManager.Execute(ListTreeCommand(self._activateDoc))
-        
-    def DirTree(self,text):
-        self._cmdManager.Execute(DirTreeCommand(self._activateDoc,text))
+
+    def DirTree(self, text):
+        self._cmdManager.Execute(DirTreeCommand(self._activateDoc, text))
 
     def History(self, log_list, log_num):
         self._cmdManager.Execute(HistoryCommand(log_list, log_num))
@@ -55,7 +58,7 @@ class TerminalApp(Application):
                 argument = ' '.join(input_list[1:])
                 filename = user_input.split(" ")[1]
                 self.session.load(filename)
-                self.session.current=filename
+                self.session.current = filename
                 self.Open(argument)
             elif command == 'save':
                 self.Save()
@@ -89,7 +92,7 @@ class TerminalApp(Application):
             elif command == "list-tree":
                 self.ListTree()
             elif command == "dir-tree":
-                if(len(input_list)==1):
+                if (len(input_list) == 1):
                     self.DirTree('')
                 else:
                     self.DirTree(input_list[1])
@@ -105,3 +108,82 @@ class TerminalApp(Application):
                 else:
                     print("Invalid stats command.")
 
+    def Test(self):
+        command_List = ["load test.md",
+                        "history 4",
+                        "list",
+                        "save",
+                        "list-tree",
+                        "dir-tree",
+                        "insert 3","list","save",
+                        "append-head","list",
+                        "append-tail","list",
+                        "delete Java从入门到入土","list",
+                        "undo","list",
+                        "redo", "list",
+                        "stats all",
+                        "Q"
+                        ]
+        for user_input in command_List:
+            time.sleep(1)
+            print("请输入命令[q\Q退出]: ", user_input)
+            # user_input = input(r"请输入命令[q\Q退出]: ")
+            if user_input == 'q' or user_input == 'Q':
+                self.session.quit()
+                break
+            self.logger.log(user_input)
+            input_list = user_input.split()
+            command = input_list[0]
+            if command == 'load':
+                argument = ' '.join(input_list[1:])
+                filename = user_input.split(" ")[1]
+                self.session.load(filename)
+                self.session.current = filename
+                self.Open(argument)
+            elif command == 'save':
+                self.Save()
+            elif command == 'insert':
+                if input_list[1].isdigit():
+                    row_num = int(input_list[1])
+                    argument = ' '.join(input_list[2:])
+                    self.InsertRow(row_num, argument)
+                else:
+                    argument = ' '.join(input_list[1:])
+                    self.InsertTail(argument)
+            elif command == 'append-head':
+                argument = ' '.join(input_list[1:])
+                self.InsertHead(argument)
+            elif command == 'append-tail':
+                argument = ' '.join(input_list[1:])
+                self.InsertTail(argument)
+            elif command == 'delete':
+                if input_list[1].isdigit():
+                    row_num = int(input_list[1])
+                    self.DeleteRow(row_num)
+                else:
+                    argument = ' '.join(input_list[1:])
+                    self.DeleteText(argument)
+            elif command == 'undo':
+                self.Undo()
+            elif command == 'redo':
+                self.Redo()
+            elif command == 'list':
+                self.List()
+            elif command == "list-tree":
+                self.ListTree()
+            elif command == "dir-tree":
+                if (len(input_list) == 1):
+                    self.DirTree('')
+                else:
+                    self.DirTree(input_list[1])
+            elif command == 'history':
+                if user_input == command:
+                    self.History(self.logger.get_content(), 0)
+                else:
+                    self.History(self.logger.get_content(), int(input_list[1]))
+            elif command == 'stats':
+                command_parts = user_input.split(" ")
+                if len(command_parts) == 2:
+                    self.session.stats(command_parts[1])
+                else:
+                    print("Invalid stats command.")
---
+++ test.py
@@ -0,0 +1,8 @@
+from TerminalApp import TerminalApp
+
+app = TerminalApp(2)
+
+print("自动化测试脚本......")
+
+app.Test()
+
