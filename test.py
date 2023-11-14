c2aa3a752467e61531e10bc6fb037ca5d5fee69f
zb2313 <2224105295@qq.com>
2023-11-14 16:15:27 +0800

    add test2.md

--- test.py
+++ test.py
@@ -1,87 +1,73 @@
 from TerminalApp import TerminalApp
 import time
-app = TerminalApp(2)
-command_List = ["load test.md",
-                        "history 4",
-                        "list",
-                        "save",
-                        "list-tree",
-                        "dir-tree",
-                        "insert 3","list","save",
-                        "append-head","list",
-                        "append-tail","list",
-                        "delete Java从入门到入土","list",
-                        "undo","list",
-                        "redo", "list",
-                        "stats all",
-                        "Q"
-                ]
-print("自动化测试脚本......")
-for user_input in command_List:
-    # 打开 txt 文件，并以读取模式打开
-    time.sleep(1)
-    print("-------------------")
-    print("请输入命令[q\Q退出]: ", user_input)
-    print("-------------------")
-    # user_input = input(r"请输入命令[q\Q退出]: ")
-    if user_input == 'q' or user_input == 'Q':
-        app.session.quit()
-        break
-    app.logger.log(user_input)
-    input_list = user_input.split()
-    command = input_list[0]
-    if command == 'load':
-        argument = ' '.join(input_list[1:])
-        filename = user_input.split(" ")[1]
-        app.session.load(filename)
-        app.session.current = filename
-        app.Open(argument)
-    elif command == 'save':
-        app.Save()
-    elif command == 'insert':
-        if input_list[1].isdigit():
-            row_num = int(input_list[1])
-            argument = ' '.join(input_list[2:])
-            app.InsertRow(row_num, argument)
-        else:
-            argument = ' '.join(input_list[1:])
-            app.InsertTail(argument)
-    elif command == 'append-head':
-        argument = ' '.join(input_list[1:])
-        app.InsertHead(argument)
-    elif command == 'append-tail':
-        argument = ' '.join(input_list[1:])
-        app.InsertTail(argument)
-    elif command == 'delete':
-        if input_list[1].isdigit():
-            row_num = int(input_list[1])
-            app.DeleteRow(row_num)
-        else:
-            argument = ' '.join(input_list[1:])
-            app.DeleteText(argument)
-    elif command == 'undo':
-        app.Undo()
-    elif command == 'redo':
-        app.Redo()
-    elif command == 'list':
-        app.List()
-    elif command == "list-tree":
-        app.ListTree()
-    elif command == "dir-tree":
-        if (len(input_list) == 1):
-            app.DirTree('')
-        else:
-            app.DirTree(input_list[1])
-    elif command == 'history':
-        if user_input == command:
-            app.History(app.logger.get_content(), 0)
-        else:
-            app.History(app.logger.get_content(), int(input_list[1]))
-    elif command == 'stats':
-        command_parts = user_input.split(" ")
-        if len(command_parts) == 2:
-            app.session.stats(command_parts[1])
-        else:
-            print("Invalid stats command.")
 
+app = TerminalApp(2)
+app2 = TerminalApp(2)
+
+print("自动化测试脚本......")
+# load
+print("load test.md")
+app.session.load("test.md")
+app.session.current = "test.md"
+app.Open("test.md")
+time.sleep(0.5)
+print("load test2.md")
+app2.session.load("test2.md")
+app2.session.current = "test2.md"
+app2.Open("test2.md")
+time.sleep(0.5)
+# save
+print("save test.md")
+app.Save()
+time.sleep(0.5)
+# insert 3
+print("insert 3 ad+ test2.md")
+app2.InsertRow(3,"ad+")
+time.sleep(0.5)
+# append-head
+print("append-head head test.md")
+app.InsertHead("head")
+time.sleep(0.5)
+# append-tail
+print("append-tail tail test2.md")
+app2.InsertTail("tail")
+time.sleep(0.5)
+# delete Java从入门到入土
+print("delete Java从入门到入土 test.md")
+app.DeleteText("Java从入门到入土")
+time.sleep(0.5)
+# undo
+print("undo")
+app.Undo()
+time.sleep(0.5)
+# redo
+print("redo")
+app.Redo()
+time.sleep(0.5)
+# list
+print("list")
+app.List()
+time.sleep(0.5)
+# list-tree
+print("list-tree")
+app.ListTree()
+time.sleep(0.5)
+# dir-tree
+print("dir-tree")
+app.DirTree('')
+time.sleep(0.5)
+# history 4
+print("history 4")
+app.History(app.logger.get_content(), 4)
+time.sleep(0.5)
+# stats all
+print("stats all")
+app.session.stats("all")
+time.sleep(0.5)
+# quit
+print("quit test.md")
+app.session.quit()
+time.sleep(0.5)
+print("quit test2.md")
+app2.session.quit()
 
